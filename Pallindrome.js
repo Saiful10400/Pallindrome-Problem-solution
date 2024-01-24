86ae0f6448923a07980867d764378beffd56db07
saiful10400 <saifulislam10400i5@gmail.com>
2024-01-24 12:30:26 +0600

    removed all console.

--- Pallindrome.js
+++ Pallindrome.js
@@ -1,26 +1,22 @@
-const isPalindrome=(text)=>{
-    const isAlphabet=/^[a-zA-Z]+$/
-    const textArray=text.split("")
-    const filteredArray=[]
-    textArray.forEach(item=>{
-        if(isAlphabet.test(item)){
-            filteredArray.push(item)
-        }
-    })
-    const reverseOfFilteredArray=[...filteredArray].reverse()
-    const baseText=filteredArray.join("").toUpperCase()
-    const reverseText=reverseOfFilteredArray.join("").toUpperCase()
-    console.log(baseText,reverseText)
-    if(baseText===reverseText){
-        return true
-
+const isPalindrome = (text) => {
+  const isAlphabet = /^[a-zA-Z]+$/;
+  const textArray = text.split("");
+  const filteredArray = [];
+  textArray.forEach((item) => {
+    if (isAlphabet.test(item)) {
+      filteredArray.push(item);
     }
-    else{
-        return false
-    }
-}
+  });
+  const reverseOfFilteredArray = [...filteredArray].reverse();
+  const baseText = filteredArray.join("").toUpperCase();
+  const reverseText = reverseOfFilteredArray.join("").toUpperCase();
 
+  if (baseText === reverseText) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-
-console.log(isPalindrome(`Madam, in Eden, I'm Adam: "Madam, in Eden, I'm Adam"`))
+console.log(isPalindrome(`("A man, a plan, a canal: Panama")`));
 // The expected result would be "true".
