01d30ab30a4fad256ea36e2c2478135dd23beb10
saiful10400 <saifulislam10400i5@gmail.com>
2024-01-24 12:23:00 +0600

    problem is solved.

---
+++ Pallindrome.js
@@ -0,0 +1,26 @@
+const isPalindrome=(text)=>{
+    const isAlphabet=/^[a-zA-Z]+$/
+    const textArray=text.split("")
+    const filteredArray=[]
+    textArray.forEach(item=>{
+        if(isAlphabet.test(item)){
+            filteredArray.push(item)
+        }
+    })
+    const reverseOfFilteredArray=[...filteredArray].reverse()
+    const baseText=filteredArray.join("").toUpperCase()
+    const reverseText=reverseOfFilteredArray.join("").toUpperCase()
+    console.log(baseText,reverseText)
+    if(baseText===reverseText){
+        return true
+
+    }
+    else{
+        return false
+    }
+}
+
+
+
+console.log(isPalindrome(`Madam, in Eden, I'm Adam: "Madam, in Eden, I'm Adam"`))
+// expected result would be "true"
